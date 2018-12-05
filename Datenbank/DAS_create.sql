7725bae0189299ef3d6b7d1e6ebfb24c479b2989
MartinHafner <martin.hafner@gmx.de>
2018-12-05 10:05:32 +0100

    resolved Database

--- Datenbank/DAS_create.sql
+++ Datenbank/DAS_create.sql
@@ -27,11 +27,12 @@ create table das_document(
   create table das_permission(
   id integer primary key,
   userid integer,
-  document integer,
+  datum date, --new
+  version integer, --version
   read varchar2(10),
   write varchar2(10),
   constraint FK_user_permission foreign key(userid) references das_user(id),
-  constraint FK_document_permission foreign key(document) references das_document(id),
+  constraint FK_version_permission foreign key(version) references das_version(id), --version
   constraint check_read check (read is not null and read in('true', 'false')),
   constraint check_write check (write is not null and write in('true', 'false'))
   );
@@ -51,20 +52,23 @@ create table das_booking(
   id integer primary key,
   userid integer,
   datum date,
-  typ varchar2(1),
-  oldversion integer,
-  version integer,
-  oldread varchar2(10),
-  read varchar2(10),
-  oldwrite varchar2(10),
-  write varchar2(10),
-  constraint FK_version_boocking foreign key(version) references das_version(id),
-  constraint FK_oldversion_boocking foreign key(oldversion) references das_version(id),
-  constraint FK_user_boocking foreign key(userid) references das_user(id),
-  constraint check_oldwrite check (write in('true', 'false')),
-  constraint check_oldread check (write in('true', 'false')),
-  constraint check_newread check (write in('true', 'false')),
-  constraint check_newwrite check (write in('true', 'false')),
-  constraint typ_check check (typ is not null and typ in('c', 'r', 'w'))
+  -- typ varchar2(1),
+  subject varchar2(50),--new
+  subjectID integer,--new
+  operation varchar2(50) --new
+  -- oldversion integer,
+  ---version integer,
+  -- oldread varchar2(10),
+  -- read varchar2(10),
+  -- oldwrite varchar2(10),
+  -- write varchar2(10),
+  -- constraint FK_version_boocking foreign key(version) references das_version(id),
+  -- constraint FK_oldversion_boocking foreign key(oldversion) references das_version(id),
+  -- constraint FK_user_boocking foreign key(userid) references das_user(id),
+  -- constraint check_oldwrite check (write in('true', 'false')),
+  -- constraint check_oldread check (write in('true', 'false')),
+  -- constraint check_newread check (write in('true', 'false')),
+  -- constraint check_newwrite check (write in('true', 'false')),
+  -- constraint typ_check check (typ is not null and typ in('c', 'r', 'w'))
 );
 create sequence boocking_sequenz MINVALUE 1 MAXVALUE 10000  START WITH 1 INCREMENT BY 1 CACHE 1000;
